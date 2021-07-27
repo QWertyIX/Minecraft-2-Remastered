9376098a0079a921604e14f7f1c978f9429318c6
QWertyIX <artem00kg@yandex.ru>
2021-07-28 00:38:30 +0600

    Теперь расчёт предыдущего кадра должен отменяться, если запрашивается новый кадр, но он не отменяется ¯\_(ツ)_/¯

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import raycomputing as rc
+import cuda_module as cum
 import tkinter as tk
 
 from PIL import Image
@@ -10,24 +11,25 @@ from copy import deepcopy
 
 import time
 
+
 data = {'camera': [0.5, 0.5, 1.5, 0.000000000001 - 0.05 * pi, 0.000000000001 - 0.15 * pi],  # позиц камеры {x,y,z, угол в xOy, угол в xOz}
         'render_distance': 10,  # глубина отрисовки в метрах (блоках) = min 10, opt 30
         'chunk_info_size': 10,  # количество блоков на одной оси чанка (кубический)
         'reflection_number': 0,  # количество отражений {0 - без отраж, луч сразу берёт цвет соответств блока}
 
         'fov_degrees': 90,  # градусов горизонтальный обзор = default 90
-        'rays_x': 16 * 5,  # лучей по горизонтали = min 16, opt 160
-        'rays_y': 9 * 5,  # лучей по вертикали = min 9, opt 90
+        'rays_x': 16 * 10,  # лучей по горизонтали = min 16, opt 160
+        'rays_y': 9 * 10,  # лучей по вертикали = min 9, opt 90
 
         'initial_screen_width': 1280,  # изначальный размер окна
 
         'world_info': {},  # координаты_чанка: чанк ('-1 13 3': chunk_info)
         'skybox': [{'general': ((176, 198, 255, 255), 0, 255, 255, (128, 128, 255))}, 100],
 
-        'texture_pack': 'digger_online_64_8',  # 64х64, 8-бит
+        # 'texture_pack': 'digger_online_64_8',  # 64х64, 8-бит
         # 'texture_pack': 'digger_online_64_32',  # 64х64, 32-бит
         # 'texture_pack': 'minecraft_16_8',  # 16х16, 8-бит
-        # 'texture_pack': 'minecraft_16_32',  # 16х16, 32-бит
+        'texture_pack': 'minecraft_16_32',  # 16х16, 32-бит
 
         # Результаты теста производительности (rays = 16x9 * 5, dist = 10, повороты камеры = s a s d w w d s a s s a):
         # minecraft        16pix    32bit    1.05 sec avg render time
@@ -42,9 +44,11 @@ data = {'camera': [0.5, 0.5, 1.5, 0.000000000001 - 0.05 * pi, 0.000000000001 - 0
                       (2, -1): ('grass_top', 0, 255, 0, 'grass_top_n'),
                       (2, 1): ('dirt', 'dirt_m', 'dirt_r', 0, 'dirt_n'),
                       'HP': 1},
+
             # блок земли (одинаков по сторонам)
             'dirt': {'general': ('dirt', 'dirt_m', 'dirt_r', 0, 'dirt_n'),
                      'HP': 2},
+
             # блок ствола дуба (сверху и снизу - срез, по краям - кора)
             'oak_log': {'general': ('oak_log', 0, 255, 0, 'oak_log_n'),
                         (2, -1): ('oak_log_top', 0, 255, 0, 'oak_log_top_n'),
@@ -66,6 +70,8 @@ avg_time_ar = []
 
 rays_x_array = []
 rays_y_array = []
+
+break_raytracing = False
 
 
 # функция первоначального задания настроек расчёта рейтрейсинга и рендеринга
@@ -90,6 +96,7 @@ def presetting_settings():
 
     data['screen'] = [window_scale, int(window_scale * data['rays_x']), int(window_scale * data['rays_y']),
                       int(data['rays_x']), int(data['rays_y'])]
+
 
 #
 # # функция создания визуальных элементов
@@ -174,6 +181,10 @@ def window_resize(event):
 
 # функция вычисления вектора перемещения и самого движения камеры
 def moving(event, speed=0.1, rotation_z=0.05 * pi, rotation_y=0.05 * pi):
+
+    global break_raytracing
+    break_raytracing = True
+
     if event.keysym == 'Up':
         if cos(data['camera'][4] + rotation_z) > 0:
             data['camera'][4] += rotation_z
@@ -193,11 +204,11 @@ def moving(event, speed=0.1, rotation_z=0.05 * pi, rotation_y=0.05 * pi):
         if event.keycode == 87:  # W
             displacement = [speed, data['camera'][3]]
 
-            global avg_time_ar
-            avg_time = 0
-            for i in avg_time_ar:
-                avg_time += i
-            print(round(avg_time / len(avg_time_ar), 2))
+            # global avg_time_ar
+            # avg_time = 0
+            # for i in avg_time_ar:
+            #     avg_time += i
+            # print(round(avg_time / len(avg_time_ar), 2))
 
         elif event.keycode == 65:  # A
             displacement = [speed, data['camera'][3] + (pi / 2)]
@@ -220,33 +231,47 @@ def moving(event, speed=0.1, rotation_z=0.05 * pi, rotation_y=0.05 * pi):
 # функция создания массива с информацией о всех пикселях
 def raytracing():
 
+    global break_raytracing
+    break_raytracing = False
+
     start = time.time()
 
     ray_array = []  # двумерный массив цветов пикселей
     for x_ray in rays_x_array:
+        if break_raytracing:
+            break
         ray_array_y = []
         for y_ray in rays_y_array:
             ray_array_y.append(rc.ray_computing(data, deepcopy(data['camera']), x_ray, y_ray))
         ray_array.append(ray_array_y)
 
-    end = time.time()
-    period = end - start
-    print('FPS:', round(1 / period, 2), '\tFrame computing time:', round(period, 2))
+    if break_raytracing:
+        end = time.time()
+        period = end - start
+        print('Raytracing broke:\n',
+              'FPS:', round(1 / period, 2), '\tFrame computing time:', round(period, 2),
+              '\n----------------------')
+    else:
+        end = time.time()
+        period = end - start
+        print('FPS:', round(1 / period, 2), '\tFrame computing time:', round(period, 2))
 
-    avg_time_ar.append(round(period, 2))
+    # avg_time_ar.append(round(period, 2))
 
     return ray_array
 
 
 # функция построения всего этого на холсте
 def rendering(ray_array):
-    canvas.delete("all")
+    if not break_raytracing:
 
-    for x in range(data['screen'][3]):
-        for y in range(data['screen'][4]):
-            canvas.create_line((x * data['screen'][0] + data['screen'][0] // 2, y * data['screen'][0]),
-                               (x * data['screen'][0] + data['screen'][0] // 2, (y + 1) * data['screen'][0]),
-                               width=data['screen'][0], fill=ray_array[x][y])
+        canvas.delete("all")
+
+        for x in range(data['screen'][3]):
+            for y in range(data['screen'][4]):
+                canvas.create_line((x * data['screen'][0] + data['screen'][0] // 2, y * data['screen'][0]),
+                                   (x * data['screen'][0] + data['screen'][0] // 2, (y + 1) * data['screen'][0]),
+                                   width=data['screen'][0], fill=ray_array[x][y])
 
 
 # функция пересчёта данных и перестроения
@@ -258,6 +283,8 @@ def play():
 if __name__ == '__main__':
     # определяем количество лучей и разрешение окна
     presetting_settings()
+
+    cum.cuda_setting_up()
 
     # # создаём окно
     # window_creating()
